bfe249ba0bcb67b12dce80bb7433f607e9a99295
Catherine <catherinezhao733@gmail.com>
2022-04-25 19:06:06 +0200

    Update register.php

--- register.php
+++ register.php
@@ -6,10 +6,12 @@ $DATABASE_PASS = 'password';
 $DATABASE_NAME = 'accounts';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
+if ($conn) {
+    echo "Success";
+} else {
     // If there is an error with the connection, stop the script and display the error.
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
+} 
 
 // Now we check if the data was submitted, isset() function will check if the data exists.
 if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
